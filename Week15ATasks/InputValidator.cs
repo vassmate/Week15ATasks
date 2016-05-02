6609d02d7f5a8f7567ff98e7be79726deabe32a4
vassmate <vassmate08@gmail.com>
2016-05-02 12:03:36 +0200

    Simplified InputValidator.cs
    Also fixed regex miss match.

--- Week15ATasks/InputValidator.cs
+++ Week15ATasks/InputValidator.cs
@@ -8,7 +8,13 @@ namespace Week15ATasks
     {
         private string notValidNameMsg = "The name is invalid (only alphabetical characters are allowed)";
         private string notValidPhoneMsg = "The phone number is not a valid US phone number";
-        private string notValidEmailMsg = "The e-mail address is not valid.";
+        private string notValidEmailMsg = "The e-mail address is not valid";
+
+        private string nameRegexPattern = @"^([A-Za-z]*\s*)*$";
+        private string phoneRegexPattern = @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$";
+        private string emailRegexPattern = @"^([a-zA-Z0-9_\-" + @"\.]+)@((\[[0-9]{1,3}" +
+                                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
 
         public InputValidator()
         {
@@ -17,34 +23,17 @@ namespace Week15ATasks
 
         private void btnSave_Click(object sender, System.EventArgs e)
         {
-            if (!Regex.IsMatch(txtName.Text, @"^([A-Za-z]*\s*)*$"))
-            {
-                MessageBox.Show(notValidNameMsg);
-            }
+            if (!Regex.IsMatch(txtName.Text, nameRegexPattern)) MessageBox.Show(notValidNameMsg);
+            if (!Regex.IsMatch(txtPhone.Text, phoneRegexPattern)) MessageBox.Show(notValidPhoneMsg);
+            if (!Regex.IsMatch(txtEmail.Text, emailRegexPattern)) MessageBox.Show(notValidEmailMsg);
 
-
-            if (!Regex.IsMatch(txtPhone.Text, @"^((\(\d{3}\)?)|(\d{3}-))?\d{3}-\d{4}$"))
-            {
-                MessageBox.Show(notValidPhoneMsg);
-                txtPhone.Text = ReformatPhone(txtPhone.Text);
-            }
-
-            if (!Regex.IsMatch(txtEmail.Text, @"^([a-zA-Z0-9_\-” + @”\.]+)@((\[[0-9]{1,3}" +
-                                              @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                                              @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
-            {
-                MessageBox.Show(notValidEmailMsg);
-            }
+            txtPhone.Text = ReformatPhone(txtPhone.Text);
         }
 
-        static string ReformatPhone(string s)
+        private string ReformatPhone(string s)
         {
-            Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
-
-            return String.Format("({0}) {1}-{2}",
-                                m.Groups[1],
-                                m.Groups[2],
-                                m.Groups[3]);
+            Match m = Regex.Match(s, phoneRegexPattern);
+            return String.Format("({0}) {1}-{2}", m.Groups[1], m.Groups[2], m.Groups[3]);
         }
     }
 }
