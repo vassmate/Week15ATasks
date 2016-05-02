5205bfd69a073c32b500a8c9c1f3aceb35359247
vassmate <vassmate08@gmail.com>
2016-05-02 13:18:04 +0200

    Single Instance application added

---
+++ SingleInstance/SingleIntsanceStart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class SingleIntsanceStart
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const string MutexName = "RUNMEONLYONCE";
+
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException ex)
+            {
+                Console.WriteLine(">Exception occurred: " + ex.Message);
+            }
+
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else{
+                oneMutex.Close();
+                return;
+            }
+
+            Console.WriteLine(">Our Application");
+            Console.Read();
+        }
+    }
+}
