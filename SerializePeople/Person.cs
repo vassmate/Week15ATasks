d593b9ae4aa23b610e25e6cfbe17d010976997ed
vassmate <vassmate08@gmail.com>
2016-05-03 10:20:14 +0200

    Initial serialization task
    Based on the summary of serialization from May 3.

---
+++ SerializePeople/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SerializePeople
+{
+    [Serializable]
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        public Person(){}
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        [OnSerializing]
+        public void ShowInfo(StreamingContext context)
+        {
+            string msg = ">I'm under serialization...";
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ SerializePeople/RunSerialization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+
+namespace SerializePeople
+{
+    static class RunSerialization
+    {
+        static void Main()
+        {
+            string nameInfo = ">Person's name: ";
+            string exitInfo = ">Press any key to exit...";
+
+            Person person = new Person("Mate", 25);
+
+            SerialzePersons(person, new SoapFormatter());
+            Person personFromFile = DeserializePersons(new SoapFormatter());
+
+            SerializePersonsToXml(person);
+
+            Console.WriteLine(nameInfo + personFromFile.Name);
+            Console.WriteLine(exitInfo);
+            Console.Read();
+        }
+
+        private static void SerializePersonsToXml(Person person)
+        {
+            XmlSerializer xmlSrlr = new XmlSerializer(typeof(Person));
+            StreamWriter strmWriter = new StreamWriter("file.xml");
+            xmlSrlr.Serialize(strmWriter, person);
+            strmWriter.Close();
+        }
+
+        private static Person DeserializePersons(IFormatter iformatter)
+        {
+            FileStream readerStream = new FileStream("file.bin", FileMode.Open);
+            Person personFromFile = (Person)iformatter.Deserialize(readerStream);
+            readerStream.Close();
+            return personFromFile;
+        }
+
+        private static void SerialzePersons(Person person, IFormatter iformatter)
+        {
+            FileStream fileStream = new FileStream("file.bin", FileMode.Create);
+            iformatter.Serialize(fileStream, person);
+            fileStream.Close();
+        }
+    }
+}
